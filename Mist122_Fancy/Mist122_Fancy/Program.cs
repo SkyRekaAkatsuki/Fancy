fecd645b9caff5d7096673bc6aa19f7cf2301b3a
stepmania003 <stepmania003@gmail.com>
2019-05-30 15:48:49 +0800

    pika

--- Mist122_Fancy/Mist122_Fancy/Program.cs
+++ Mist122_Fancy/Mist122_Fancy/Program.cs
@@ -16,7 +16,7 @@ namespace Mist122_Fancy
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new TestPush());
         }
     }
 }
