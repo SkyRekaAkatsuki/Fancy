7a23238a8e8b3f9b8205f0b79078a3d410592205
stepmania003 <stepmania003@gmail.com>
2019-05-30 17:26:58 +0800

    DBEntity
    重放

--- FancyStore/DB_Fancy/Properties/AssemblyInfo.cs
+++ FancyStore/DB_Fancy/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
-[assembly: Guid("2ca44fab-cec4-4fa9-83de-f982302e21de")]
+[assembly: Guid("a2c5b8b5-4da9-4d3c-b411-c954fbebdffc")]
 
 // 組件的版本資訊由下列四個值所組成: 
 //
